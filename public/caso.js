98cf948a11bd0f0fcd5d773000902ffcc119d3b1
emorillo <elisaulmorillo@gmail.com>
2018-07-30 08:02:19 -0400

    Saving user if does not exits

--- public/caso.js
+++ public/caso.js
@@ -4,9 +4,16 @@ window.onload = function (e) {
 }
 
 
-function DetalleCaso(item) {
-    alert(item)
-    var locref = firebase.database.ref('Casos').child(item);
+function DetalleCaso(i) {
+    // alert(item)
+    frebase.database.ref('Casos').child(i)
+        .once('value')
+        .then(function (snapshot) {
+            var value = snapshot.val();
+            console.log('Casos:', value.Usuario);
+            resp.json(value.Usuario);
+        })
+        .catch(next);
     //const location = $firebaseObject(item);
     //alert(location)
     $("#myModal").modal('show');
@@ -42,8 +49,9 @@ function CargarCasos() {
                 td = _dce('td');
                 td.innerHTML = "<image  style='height:80px' src=" + item.val().Evidencia + " >"
                 tr.appendChild(td);
+
                 td = _dce("td");
-                td.innerHTML = '<button data-toggle="modal" data-target="#myModal" class="btn btn-success" id="btn" name="btn" onclick=DetalleCaso(' + item.key + ')>Ver Detalle</button>';
+                td.innerHTML = '<button data-toggle="modal" data-target="#myModal" class="btn btn-success"  onclick=DetalleCaso(' + item.key + ')>Ver Detalle</button>';
                 tr.appendChild(td);
                 destino.appendChild(tr);
             });
--- public/login.js
+++ public/login.js
@@ -10,7 +10,7 @@ firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         var user = firebase.auth().currentUser;
         if (user != null) {
-             
+
             if (user.email == "admin@gmail.com") {
                 window.location = "Configuracion.html";
             } else {
@@ -18,7 +18,7 @@ firebase.auth().onAuthStateChanged(function (user) {
             }
         }
     } else {
-       // window.location = "Login.html";
+        // window.location = "Login.html";
     }
     // ...
 });
@@ -28,25 +28,24 @@ function LoginUser() {
     var email = document.getElementById("UserName").value
     var password = document.getElementById("Password").value;
 
-    // firebase.auth().signInWithEmailAndPassword(email, password)
-    //     .then((authData) => {
-    //         console.log("User created successfully with payload-", authData);
-    //         // alert(authData)
-    //         var user = firebase.auth().currentUser;
-    //     }).catch((_error) => {
-    //         console.log("Login Failed!", _error);
-    //     })
-    // firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     window.alert(errorMessage)
-    // });
+
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here.
+
         var errorCode = error.code;
         var errorMessage = error.message;
-        window.alert(errorMessage)
+        if (errorCode == "auth/user-not-found") {
+            var result = confirm("Usuario no registrado se procedera a crear nuevo");
+            if (result == true) {
+                firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+
+                    var erMsg = error.message;
+                    window.alert(erMsg)
+                });
+            }
+        } else {
+            window.alert(errorMessage) //+ " cdde " + errorCode)
+        }
+
     });
 }
 
